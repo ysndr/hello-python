b11d3248277e69c745e6578372a92ce086b2f4dc
Michael Brantley <limeytexan@users.noreply.github.com>
2020-11-25 11:10:10 +0000

    Add dependency on hello-python-library package

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="hello-python",
     version="0.1",
     packages=find_packages(),
+    install_requires = [ 'hello_python_library' ],
     entry_points={
         'console_scripts': [
             'hello = hello:hello',
